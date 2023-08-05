a0210eb36cb67f734f1053555ab200201f41916d
Ankit68543 <ankit68543@gmail.com>
2023-08-05 13:47:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExecutableCICD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine("Hello World => " + i);
+            }
+
+            Environment.Exit(1);
+        }
+    }
+}
